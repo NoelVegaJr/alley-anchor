b74740241ca5743eca61c172e64631da3c42403b
noelvegacodes <noelvegacodes@gmail.com>
2023-06-02 17:50:36 -0400

    hero cat button

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -14,7 +14,7 @@ export default function NavLink({
     <Link
       href={href}
       target={`${newTab && "_blank"}`}
-      className={`w-full uppercase   hover:bg-sea/80 text-gray-100  tracking-wider block bg-sea xl:bg-transparent
+      className={`w-full uppercase   bg-sea/80 hover:bg-sea text-gray-100  tracking-wider block 
       hover:text-gray-100 border-2 border-sea  rounded py-2 text-center drop-shadow-2xl  font-extrabold transition-all duration-300 ${garamond.className}`}
     >
       {children}
