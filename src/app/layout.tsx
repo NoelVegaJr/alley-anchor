cfee18000d0d87c988ffeae61e09e3ea2feb4cd1
noelvegacodes <noelvegacodes@gmail.com>
2023-06-09 21:23:46 -0400

    updated title img for tab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full w-full">
+      <head>
+        <link rel="icon" href="/logo.png" />
+      </head>
       <body className="h-screen w-full">{children}</body>
     </html>
   );
